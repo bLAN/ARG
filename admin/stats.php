cd0091565ae2d7bc3966360e8efdf6b4200968e5
erviker <erviker@gmail.com>
2016-11-21 19:46:44 +0100

    html og include

---
+++ admin/stats.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once '../medoo.php';
+include "../config.php";
+include "../function.php";
+
+?>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap-superhero.css">
+        <link rel="stylesheet" href="bootstrap/css/jumbotron-narrow.css">
+        <script src="js/jquery-3.1.1.min.js"></script>
+        <script src="bootstrap/js/bootstrap.js"></script>
+        <title><?php echo $config["name"];?></title>
+    </head>
+    <body>
+        <div class="container">
+
+<?php
+
+
+
+?>
+
+
+</div>
+</body></title>
+</head>
+</html>
